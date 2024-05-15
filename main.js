fa7c040c43fbbe50c4c7c80677930ce0ca0b434d
Cato <151020503+CatfishGG@users.noreply.github.com>
2024-05-15 22:02:19 +1000

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, ipcMain, dialog } = require('electron');
 const path = require('path');
+const fs = require('fs');
+const https = require('https');
 
 function createWindow() {
     const win = new BrowserWindow({
@@ -25,5 +27,29 @@ app.on('window-all-closed', () => {
 app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) {
         createWindow();
+    }
+});
+
+ipcMain.on('download-photo', async (event, url) => {
+    const win = BrowserWindow.getFocusedWindow();
+    const options = {
+        title: 'Save Image',
+        defaultPath: path.join(app.getPath('downloads'), 'photo.jpg'),
+        buttonLabel: 'Save',
+        filters: [{ name: 'Images', extensions: ['jpg', 'png', 'gif'] }]
+    };
+    const { filePath } = await dialog.showSaveDialog(win, options);
+    if (filePath) {
+        const file = fs.createWriteStream(filePath);
+        https.get(url, (response) => {
+            response.pipe(file);
+            file.on('finish', () => {
+                file.close();
+                console.log('Download completed!');
+            });
+        }).on('error', (err) => {
+            fs.unlink(filePath, () => {});
+            console.error('Error downloading file:', err);
+        });
     }
 });
